3a0b2725aa771b246a70084e0bf4365ea8bb5fd1
EthMoab <ethmoab@gmail.com>
2025-05-20 20:15:03 +0700

    Deploy PreDeposits

--- contracts/USDe.sol
+++ contracts/USDe.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title USDe
- * @notice USDe Genesis Story: Arthur Hayes' $Nakadollar in "Dust on Crust" 08/03/2023
+ * @notice USDe placeholder contract used just for testing and development puposes.
  */
 contract USDe is ERC20 {
 
